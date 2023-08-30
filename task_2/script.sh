497c11c9f42a0e29b663a7a1a0b04081091d3ea1
maksaptz <128064182+maksaptz@users.noreply.github.com>
2023-08-30 20:16:35 +0300

    Update script.sh

--- task_2/script.sh
+++ task_2/script.sh
@@ -3,12 +3,9 @@
 ### обновим менеджер пакетов и установим пакеты необходимые для работы
 apt update
 apt install docker.io -y
-
+###Создадим пользователя и добавим его в группу docker которая позволяет управлять dockerom
 useradd user -s /bin/bash -G docker
-
+###зададим пароль
 echo "user:user" | chpasswd
-
+###добавим дополнительный модуль в sudoers который позволит рестартить сервис
 echo "user ALL= NOPASSWD: /bin/systemctl restart docker.service" > /etc/sudoers.d/user
-
-### sudo systemctl restart docker.service
-### docker ps
